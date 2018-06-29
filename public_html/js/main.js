488c4cf6c00785ca52b95de472dfc2e9068aae61
Dawid Wesolowski <dawaw7@wp.pl>
2018-06-30 00:47:27 +0200

    Wszystkie kierunki

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -5,12 +5,45 @@
  */
 
 let square = document.querySelector('#main');
-square.addEventListener("mousemove", (e)=>{
-   console.log(square.getBoundingClientRect());
-   //let position= square.getBoundingClientRect();
+square.addEventListener("mouseover", (e)=>{
+   let square2 = document.querySelector('#square');
+   //console.log(square.getBoundingClientRect());
+   let position= square2.getBoundingClientRect();
+//   console.log(position.bottom);
+//   console.log(e.clientY);
+    if (e.clientX<position.left && e.clientY>position.top && e.clientY<position.bottom) {
+    square.style.left = e.clientX+5+"px";
+    console.log("udało się 2");
+   }else if (e.clientX>position.right && e.clientY<position.bottom && e.clientY>position.top) {
+      square.style.left = e.clientX-85+"px";
+//      console.log(position.left);
+//   console.log(e.clientX);
+   }else if (e.clientX>position.left && e.clientX<position.right && e.clientY>position.bottom) {
+         square.style.top = e.clientY-85+"px";
+      
+   }else  if (e.clientX>position.left && e.clientX<position.right && e.clientY<position.top) {
+        square.style.top = e.clientY+5+"px";
+      
+   }else if (e.clientX<position.left && e.clientY<position.top){
+     square.style.left = e.clientX+5+"px";
+     square.style.top = e.clientY+5+"px";
+   }else if (e.clientX>position.right && e.clientY<position.top){
+      square.style.left = e.clientX-85+"px";
+     square.style.top = e.clientY+5+"px";
+   }else  if (e.clientX>position.right && e.clientY>position.bottom) {
+         square.style.top = e.clientY-85+"px";
+         square.style.left = e.clientX-85+"px";
+   }else  if (e.clientX<position.left && e.clientY>position.bottom) {
+         square.style.top = e.clientY-85+"px";
+         square.style.left = e.clientX+5+"px";
+   }
+   
+
+   
+
    //square.style.right = e.clientY+"px";
    //square.style.left = e.clientX+"px";
-   square.style.left = e.clientX+5+"px";
+  
 //   document.addEventListener('mousemove', (e)=>{
 //   square.style.left = e.clientX+"px";
 //   });
@@ -21,6 +54,6 @@ square.addEventListener("mousemove", (e)=>{
  /**
   * Comment
   */
-function rotate(element) {
-   element.style
-}
+//function rotate(element) {
+//   
+//}

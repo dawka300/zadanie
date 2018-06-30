a2b3036d70a14b7f34b0c4f88663ec790025c094
Dawid Wesolowski <dawaw7@wp.pl>
2018-07-01 00:26:48 +0200

    Finish

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -5,52 +5,98 @@
  */
 
 let square = document.querySelector('#main');
-square.addEventListener("mouseover", (e)=>{
-   let square2 = document.querySelector('#square');
-   //console.log(square.getBoundingClientRect());
-   let position= square2.getBoundingClientRect();
-//   console.log(position.bottom);
-//   console.log(e.clientY);
-    if (e.clientX<position.left && e.clientY>position.top && e.clientY<position.bottom) {
-    square.style.left = e.clientX+5+"px";
-    console.log("udało się 2");
-   }else if (e.clientX>position.right && e.clientY<position.bottom && e.clientY>position.top) {
-      square.style.left = e.clientX-85+"px";
-//      console.log(position.left);
-//   console.log(e.clientX);
-   }else if (e.clientX>position.left && e.clientX<position.right && e.clientY>position.bottom) {
-         square.style.top = e.clientY-85+"px";
-      
-   }else  if (e.clientX>position.left && e.clientX<position.right && e.clientY<position.top) {
-        square.style.top = e.clientY+5+"px";
-      
-   }else if (e.clientX<position.left && e.clientY<position.top){
-     square.style.left = e.clientX+5+"px";
-     square.style.top = e.clientY+5+"px";
-   }else if (e.clientX>position.right && e.clientY<position.top){
-      square.style.left = e.clientX-85+"px";
-     square.style.top = e.clientY+5+"px";
-   }else  if (e.clientX>position.right && e.clientY>position.bottom) {
-         square.style.top = e.clientY-85+"px";
-         square.style.left = e.clientX-85+"px";
-   }else  if (e.clientX<position.left && e.clientY>position.bottom) {
-         square.style.top = e.clientY-85+"px";
-         square.style.left = e.clientX+5+"px";
+let left = 50;
+let tops = 50;
+let transf=0;
+let change=false;
+let rot=false;
+document.querySelector('#color').addEventListener("change", ()=>{
+   if (document.querySelector('#color').checked) {
+     change=true;
+   }else {
+      change=false
    }
-   
 
-   
-
-   //square.style.right = e.clientY+"px";
-   //square.style.left = e.clientX+"px";
-  
-//   document.addEventListener('mousemove', (e)=>{
-//   square.style.left = e.clientX+"px";
-//   });
- //  console.log(e.pageX);
-  // console.log(position.x);
    
 });
+document.querySelector('#rotat').addEventListener("change", ()=>{
+   if (document.querySelector('#rotat').checked) {
+     rot=true;
+   }else {
+      rot=false
+   }
+
+   
+});
+square.addEventListener("mouseover", (e)=>{
+   let square2 = document.querySelector('#square');
+   if (change) {
+      changeColor(square2);
+   }
+   if (rot) {
+      rotate(square2, 15);
+   }
+   
+   let position2= square2.getBoundingClientRect();
+   let skok=2;
+ if (e.pageX<position2.left && e.pageY>position2.top && e.pageY<position2.bottom) {
+    left +=skok;
+     square.style.left = left+"%";
+  
+   }else if (e.pageX>position2.right && e.pageY<position2.bottom && e.pageY>position2.top) {
+      left -=skok;
+     
+      square.style.left = left+"%";
+
+   }else if (e.pageX>position2.left && e.pageX<position2.right && e.pageY>position2.bottom) {
+      tops -= skok;
+         square.style.top = tops+"%";
+      
+   }else  if (e.pageX>position2.left && e.pageX<position2.right && e.pageY<position2.top) {
+        tops += skok;
+         square.style.top = tops+"%";
+      
+   }else if (e.pageX<position2.left && e.pageY<position2.top){
+    tops += skok;
+    left += skok;
+    square.style.top = tops+"%";
+    square.style.left = left+"%";
+   }else if (e.pageX>position2.right && e.pageY<position2.top){
+      tops +=skok;
+    left -=skok;
+    square.style.top = tops+"%";
+    square.style.left = left+"%";
+    
+   }else  if (e.pageX>position2.right && e.pageY>position2.bottom) {
+         tops -= skok;
+    left -=skok;
+    square.style.top = tops+"%";
+    square.style.left = left+"%";
+   
+   }else  if (e.pageX<position2.left && e.pageY>position2.bottom) {
+         tops -= skok;
+    left +=skok;
+    square.style.top = tops+"%";
+    square.style.left = left+"%";
+   }   
+});
+/**
+ * Zmiana koloru
+ */
+function changeColor(el) {
+   
+   var a = Math.floor(Math.random()*256);
+   var b = Math.floor(Math.random()*256);
+   var c = Math.floor(Math.random()*256);
+   
+   el.style.backgroundColor = `rgb(${a}, ${b}, ${c})`;
+   
+}
+
+function rotate(el, skok){
+   transf+=skok;
+   el.style.transform = `rotate(${transf}deg)`;
+}
  /**
   * Comment
   */
